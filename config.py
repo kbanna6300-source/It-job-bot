ede0357cb13dfc3ef705b094f8aef0a4a4dfed2c
kbanna6300-source <kbanna6300@gmail.com>
2026-07-31 15:14:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+TELEGRAM_BOT_TOKEN = os.getenv("BOT_TOKEN")
+TELEGRAM_CHANNEL_ID = os.getenv("CHANNEL_ID")
+
+JOB_KEYWORDS = [
+    "Desktop Support",
+    "Technical Support",
+    "Service Desk",
+    "IT Helpdesk",
+    "L1 Support",
+    "L2 Support"
+]
